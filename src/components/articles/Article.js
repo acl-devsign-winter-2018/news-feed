aa6e513555543487fd5405335b6c80bf7f70356d
G.Provost <grace.g.provost@gmail.com>
2018-01-12 12:01:55 -0800

    add rendering to Article.js

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -8,4 +8,15 @@ export default class Article {
   constructor(articleInfo) {
     this.articleInfo = articleInfo;
   }
+
+  render() {
+    const dom = template.render();
+    const articleInfo = this.articleInfo;
+
+    dom.querySelector('.title').textContent = articleInfo.title;
+    dom.querySelector('.author').textContent = articleInfo.author ? articleInfo.author : "";
+    dom.querySelector('.description').textContent = articleInfo.description ? articleInfo.description : "";
+    dom.querySelector('.thumbnail').setAttribute('src', `${articleInfo.urlToImage ? articleInfo.urlToImage : ""}`);
+    dom.querySelector('.thumbnail').setAttribute('alt', articleInfo.title);
+  }
 }
