67d91465bda6c1b5ad68bc60df39ae5f12c4bb7a
erdem koral <koral.erdem@gmail.com>
2018-01-12 12:24:18 -0800

    create newsApi file, fetch works

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,1 +1,8 @@
+const KEY = 'f36e77c8c5ce43cbba8790773ba56fe8';
+const NEWS_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${KEY}`;
 
+// const storage = window.localStorage;
+
+fetch(NEWS_URL)
+  .then(response => response.json())
+  .then(data => console.log(data));
