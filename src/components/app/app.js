ce64efc376e35f1789bf6c27a9ffc867170b1137
mewarren <holla@mewarren.com>
2018-01-12 10:55:30 -0800

    render search bar to DOM

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,18 @@
 import Template from '../Template';
 import html from './app.html';
+import Search from '../search/Search';
 
 const template = new Template(html);
 
 export default class App{
   render(){
-    return template.render();
+    const dom = template.render();
+    
+    const search = new Search();
+    const searchBox = dom.getElementById('search');
+    searchBox.appendChild(search.render());
+
+
+    return dom;
   }
 }
