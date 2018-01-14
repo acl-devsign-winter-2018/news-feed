c3320f84c94e4441b5c5b0da4ea8b393fa9ad00e
Jennifer Elton <justjenny40@hotmail.com>
2018-01-14 14:40:47 -0800

    added search feature to the news page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,17 @@
 import html from './app.html';
 import './app.css';
 import Template from '../Template';
+import Search from '../search/Search';
 
 const template = new Template(html);
 
 export default class App {
   render() {
-    return template.render();
+    const dom = template.render();
+    const searchSection = dom.getElementById('search');
+
+    const search = new Search();
+    searchSection.appendChild(search.render());
+    return dom;
   }
 }
