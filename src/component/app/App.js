1790d3e668e90756c40109ad612eb3aa1d1b41cf
Morgan LaCouture <morganlea18@gmail.com>
2018-01-14 15:17:40 -0800

    search event working

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -6,12 +6,17 @@ import Search from '../search/Search';
 const template = new Template(html);
 
 export default class App {
+
+  handleSearch(searchTerm) {
+    console.log('SEARCH:', searchTerm);
+  }
   render() {
     const dom = template.render();
     const searchSection = dom.getElementById('search');
 
-    const search = new Search();
+    const search = new Search(searchTerm => this.handleSearch(searchTerm));
     searchSection.appendChild(search.render());
+
 
     return dom;
   }
--- src/component/search/Search.js
+++ src/component/search/Search.js
@@ -5,9 +5,21 @@ import Template from '../Template';
 const template = new Template(html);
 
 export default class Search {
+  constructor(onSearch) {
+    this.onSearch = onSearch;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.onSearch(this.searchInput.value);
+  }
 
   render() {
     const dom = template.render();
+    this.searchInput = dom.querySelector('input');
+
+    const form = dom.querySelector('form');
+    form.addEventListener('submit', event => this.handleSubmit(event));
 
     return dom;
   }
