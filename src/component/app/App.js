94584ab8173c9b7c2365c99b2de196798d6e2593
Morgan LaCouture <morganlea18@gmail.com>
2018-01-15 14:54:49 -0800

    cannont get API info appended

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -2,6 +2,7 @@ import Template from '../Template';
 import html from './app.html';
 import './app.css';
 import Search from '../search/Search';
+import NewsFeed from '../news/NewsFeed';
 import { searchNews } from '../../service/newsApi';
 
 const template = new Template(html);
@@ -11,14 +12,28 @@ export default class App {
   handleSearch(searchTerm) {
     searchNews(searchTerm)
       .then(data => {
+        const news = data.items;
+        const total = data.totalResults;
+
         console.log(data);
+
+        const newsSection = this.newsSection;
+
+        while(newsSection.hasChildNodes()) {
+          newsSection.removeChild(newsSection.lastChild());
+
+          const newsFeed= new NewsFeed(news);
+          newsSection.appendChild(newsFeed.render());
+        }
       });
 
   }
   render() {
     const dom = template.render();
-    const searchSection = dom.getElementById('search');
 
+    this.newsSection = dom.getElementById('news');
+
+    const searchSection = dom.getElementById('search');
     const search = new Search(searchTerm => this.handleSearch(searchTerm));
     searchSection.appendChild(search.render());
 
--- src/service/newsApi.js
+++ src/service/newsApi.js
@@ -1,10 +1,22 @@
 const KEY = '3a73eda8828f4f74a3e66753884a13a8';
 const NEWS_URL = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${KEY}`;
 
+const storage = window.localStorage;
+
 export function searchNews(searchTerm){
-  return fetch(`${NEWS_URL}&q=${searchTerm}&maxResults=40`)
+  const url = `${NEWS_URL}&q=${searchTerm}&maxResults=40`;
   //defaults to 10 bumping it up to 40
-  
-    .then(response => response.json());
+
+  // .then(response => response.json());
+
+  const data = storage.getItem(url);
+  if(data) return Promise.resolve(JSON.parse(data));
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      storage.setItem(url, JSON.stringify(data));
+      return data;
+    });
 
 }
