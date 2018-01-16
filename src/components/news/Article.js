e6a42d3f5a371c73d8e093aafb35083e2d6cc1d4
Zac-L <Zachlichens@gmail.com>
2018-01-16 11:52:20 -0800

    fixed indent issue

--- src/components/news/Article.js
+++ src/components/news/Article.js
@@ -5,19 +5,19 @@ import Template from '../Template';
 const template = new Template(html);
 
 export default class Article {
-    constructor(article) {
-        this.article = article.articles;
-    }
+  constructor(article) {
+    this.article = article.articles;
+  }
 
-    render() {
-        const dom = template.render();
-        const article = this.article;
+  render() {
+    const dom = template.render();
+    const article = this.article;
 
-        dom.querySelector('.title').textContent = article.title;
-        dom.querySelector('.author').textContent = article.author;
-        dom.querySelector('.publisher').textContent = article.publisher;
-        dom.querySelector('.publishedDate').textContent = article.publishedDate;
-        dom.querySelector('.description').textContent = article.description;
+    dom.querySelector('.title').textContent = article.title;
+    dom.querySelector('.author').textContent = article.author;
+    dom.querySelector('.publisher').textContent = article.publisher;
+    dom.querySelector('.publishedDate').textContent = article.publishedDate;
+    dom.querySelector('.description').textContent = article.description;
         
-    }
+  }
 }
