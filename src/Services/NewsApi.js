052ce099c5e8bee73201e9539bd78dc79a0f131a
Jennifer Elton <justjenny40@hotmail.com>
2018-01-16 12:32:09 -0800

    got the API to render to  the page, started on paging functionality, did not get done

--- src/Services/NewsApi.js
+++ src/Services/NewsApi.js
@@ -1,8 +1,18 @@
 const KEY = '1d3c73c5e4ce4ae0bf9f91555bd28bed';
-const NEWS_URL = `https://newsapi.org/v2/top-headlines?sources=hacker-news&apiKey=${KEY}`;
+const NEWS_URL = `https://newsapi.org/v2/top-headlines?apiKey=${KEY}`;
+
+const storage = window.localStorage;
 
 export function searchNews(searchTerm) {
-  return fetch(`${NEWS_URL}&q=${searchTerm}&maxResults=40`) 
-    .then(response => response.json());
-    
+  const url = `${NEWS_URL}&q=${searchTerm}&maxResults=40`;
+
+  const data =  storage.getItem(url);
+  if(data) return Promise.resolve(JSON.parse(data));
+
+  return fetch(url)
+    .then(response => response.json())
+    .then (data => {
+      storage.setItem(url, JSON.stringify(data));
+      return data;
+    });
 }
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,8 @@ import Template from '../Template';
 import html from './app.html';
 import './app.css';
 import Search from '../search/Search';
+import Paging from '../search/Paging';
+import NewsFeed from '../news/NewsFeed';
 import { searchNews } from '../../services/newsApi';
 
 const template = new Template(html);
@@ -11,17 +13,31 @@ export default class App {
   handleSearch(searchTerm) {
     searchNews(searchTerm)
       .then(data => {
-        console.log(data);
+        
+        const news = data.articles;
+        const total = data.totalResults;
+        
+        const newsSection = this.newsSection;
+
+        while(newsSection.hasChildNodes()) {
+          newsSection.removeChild(newsSection.lastChild());
+        }
+        const newsFeed = new NewsFeed(news);
+        newsSection.appendChild(newsFeed.render());
       });
     
   }
   render() {
     const dom = template.render();
+    this.newsSection = dom.getElementById('news');
     const searchSection = dom.getElementById('search');
 
     const search = new Search(searchTerm => this.handleSearch(searchTerm));
     searchSection.appendChild(search.render());
 
+    const pagingSection = dom.getElementById('paging');
+    const paging = new Paging(pageIndex => this.handlePaging(pageIndex));
+    pagingSection.appenChild(paging.render());
     return dom;
   }
 }
